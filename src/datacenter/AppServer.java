c908aa03fa4447c862d41b23b12f0951a473af8e
Shenghan Gao <victorgaohk@gmail.com>
2016-04-25 21:30:33 -0700

    Modify

--- src/datacenter/AppServer.java
+++ src/datacenter/AppServer.java
@@ -40,36 +40,35 @@ public class AppServer {
 	private List<String> datacenterIP;
 
 	public AppServer() {
-		this(3);
+		// this(3);
 		int cnt = 2;
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
 				this.timeTable[i][j] = cnt++;
 			}
 		}
-		Operation post = new PostOperation("Message 1");
-		Event e = new Event(post, 1, 1);
-		log.add(e);
-		post = new PostOperation("Message 2");
-		e = new Event(post, 2, 2);
-		log.add(e);
-		messages.add("Message1");
-		messages.add("Message2");
-		messages.add("Message3");
-		messages.add("Message4");
+		// Operation post = new PostOperation("Message 1");
+		// Event e = new Event(post, 1, 1);
+		// log.add(e);
+		// post = new PostOperation("Message 2");
+		// e = new Event(post, 2, 2);
+		// log.add(e);
+		// messages.add("Message1");
+		// messages.add("Message2");
+		// messages.add("Message3");
+		// messages.add("Message4");
 	}
 
-	public AppServer(int numOfDC) {
+	public AppServer(int numOfDC, List<String> datacenterIPs) {
 		this.timeTable = new int[numOfDC][numOfDC];
 		this.log = new ArrayList<>();
 		this.messages = new ArrayList<>();
 		this.clientIP = new ArrayList<>();
-		this.datacenterIP = new ArrayList<>();
-		System.out.println("My nodeId is " + AppServer.nodeId);
+		this.datacenterIP = new ArrayList<>(datacenterIPs);
 	}
 
 	private synchronized void handleClientsReq(String req, Socket socket) {
-		System.out.println(req);
+		// System.out.println(req);
 
 		String[] ss = req.split(" ", 2);
 		if (ss[0].compareTo("p") == 0) {
@@ -77,7 +76,7 @@ public class AppServer {
 		} else if (ss[0].compareTo("l") == 0) {
 			lookup(socket);
 		} else if (ss[0].compareTo("s") == 0) {
-
+			sync(1);
 		}
 	}
 
@@ -102,16 +101,16 @@ public class AppServer {
 		}
 	}
 
-	private void sync(String desIP) {
-		// System.out.println("RequestForSync!");
-		// Runnable r = new SyncWithDCThread(0);
-		// Thread syncWithDCThread = new Thread(r);
-		// syncWithDCThread.start();
-		// try {
-		// syncWithDCThread.join();
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
+	private void sync(int des) {
+		System.out.println("RequestForSync!");
+		Runnable r = new SyncWithDCThread(des);
+		Thread syncWithDCThread = new Thread(r);
+		syncWithDCThread.start();
+		try {
+			syncWithDCThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static boolean hasReceive(int[][] timeTable, int id, int eventNode, int eventTime) {
@@ -173,38 +172,69 @@ public class AppServer {
 	public static void main(String[] args) throws UnknownHostException {
 		// int numOfDC = Integer.parseInt(args[1]);
 
-		AppServer.nodeId = Integer.parseInt(args[0]);
-		appServer = new AppServer();
+		// AppServer.nodeId = Integer.parseInt(args[0]);
+		// appServer = new AppServer(2);
 
-		appServer.datacenterIP.add("127.0.0.1");
+		List<String> datacenterIPs = new ArrayList<>();
+		datacenterIPs.add("169.231.28.43");
+		datacenterIPs.add("128.111.43.39");
+		datacenterIPs.add("128.111.43.56");
+
+		InetAddress inetAddress = InetAddress.getLocalHost();
+		String IPAddress = inetAddress.getHostAddress();
+
+		// System.out.println("IPAddress = " + IPAddress);
+
+		appServer = new AppServer(datacenterIPs.size(), datacenterIPs);
+
+		int k = 0;
+		for (; k < appServer.datacenterIP.size(); ++k) {
+			if (IPAddress.compareTo(appServer.datacenterIP.get(k)) == 0)
+				break;
+		}
+		if (k == appServer.datacenterIP.size()) {
+			System.out.println("The IP of this machine is not in the list!");
+			return;
+		}
+		AppServer.nodeId = k + 1;
+		System.out.println("My nodeId is " + AppServer.nodeId);
 
 		Thread listenToClientsThread = new Thread(new ListenToClientsThread());
 		listenToClientsThread.start();
 
-		boolean requester = false;
-		if (requester) {
-			System.out.println("RequestForSync!");
-			Runnable r = new SyncWithDCThread(0);
-			Thread syncWithDCThread = new Thread(r);
-			syncWithDCThread.start();
-			try {
-				syncWithDCThread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		// boolean requester = false;
+		// if (requester) {
+		// System.out.println("RequestForSync!");
+		// Runnable r = new SyncWithDCThread(0);
+		// Thread syncWithDCThread = new Thread(r);
+		// syncWithDCThread.start();
+		// try {
+		// syncWithDCThread.join();
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+		//
+		// } else {
+		// System.out.println("Server!");
+		// Thread listenToDCThread = new ListenToDCThread();
+		// listenToDCThread.start();
+		// try {
+		// listenToDCThread.join();
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// try {
+		// listenToClientsThread.join();
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
 
-		} else {
-			System.out.println("Server!");
-			Thread listenToDCThread = new ListenToDCThread();
-			listenToDCThread.start();
-			try {
-				listenToDCThread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+		System.out.println("Server!");
+		Thread listenToDCThread = new ListenToDCThread();
+		listenToDCThread.start();
 		try {
-			listenToClientsThread.join();
+			listenToDCThread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -286,7 +316,7 @@ public class AppServer {
 					e.printStackTrace();
 				}
 
-				System.out.println("End of ListenToClientsSocketHandler!");
+				// System.out.println("End of ListenToClientsSocketHandler!");
 			}
 		}
 	}
@@ -326,13 +356,16 @@ public class AppServer {
 					InputStreamReader isr = new InputStreamReader(connectedSocket.getInputStream());
 					BufferedReader br = new BufferedReader(isr);
 					String signal = br.readLine();
-					System.out.println("signal = " + signal);
+					int targetId = Integer.parseInt(br.readLine());
+					// System.out.println("signal = " + signal);
+
+					System.out.println("targetId = " + targetId);
 
 					ObjectOutputStream oos = new ObjectOutputStream(connectedSocket.getOutputStream());
 
-					// SyncData syncData = send(targetId,
-					// AppServer.appServer.log, AppServer.appServer.timeTable);
-					SyncData syncData = send(0, AppServer.appServer.log, AppServer.appServer.timeTable);
+					SyncData syncData = send(targetId, AppServer.appServer.log, AppServer.appServer.timeTable);
+					// SyncData syncData = send(0, AppServer.appServer.log,
+					// AppServer.appServer.timeTable);
 
 					oos.writeObject(syncData);
 					oos.flush();
@@ -368,7 +401,8 @@ public class AppServer {
 			// desAddress =
 			// InetAddress.getByName(AppServer.this.datacenterIP.get(des));
 			try {
-				desAddress = InetAddress.getByName("127.0.0.1");
+				// desAddress = InetAddress.getByName("127.0.0.1");
+				desAddress = InetAddress.getByName(AppServer.appServer.datacenterIP.get(des - 1));
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
@@ -388,6 +422,7 @@ public class AppServer {
 				e.printStackTrace();
 			}
 			pw.println("S");
+			pw.println(AppServer.nodeId);
 			pw.flush();
 
 			try {
