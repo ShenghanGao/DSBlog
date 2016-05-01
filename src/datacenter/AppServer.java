03cedf09a0c5797ad6f311dec3cba952fe8f2ed3
Shenghan <victorgaohk@gmail.com>
2016-05-01 15:44:01 -0700

    Change IP

--- src/datacenter/AppServer.java
+++ src/datacenter/AppServer.java
@@ -147,10 +147,12 @@ public class AppServer {
 	public static void main(String[] args) throws UnknownHostException {
 		List<String> datacenterIPs = new ArrayList<>();
 
-		datacenterIPs.add("128.111.43.40"); // thundarr.cs.ucsb.edu
+		// datacenterIPs.add("128.111.43.40"); // thundarr.cs.ucsb.edu
 		// datacenterIPs.add("128.111.43.41"); //optimus.cs.ucsb.edu
 		// datacenterIPs.add("128.111.43.42"); //megatron.cs.ucsb.edu
-		datacenterIPs.add("169.231.10.153");
+		datacenterIPs.add("128.111.43.56");
+		datacenterIPs.add("128.111.84.203");
+		datacenterIPs.add("128.111.84.167");
 
 		int numOfNodes = datacenterIPs.size();
 
